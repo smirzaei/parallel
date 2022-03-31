c66e88f6a48671904d717e2dc93a2070db5f9ad3
Soroush Mirzaei <soroush.mirzaei@gmail.com>
2022-03-31 10:29:34 +0430

    Testing ForEach to work fine concurrently.

--- parallel_test.go
+++ parallel_test.go
@@ -1,6 +1,7 @@
 package parallel
 
 import (
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -20,6 +21,55 @@ func TestForEachToCallTheFunctionForEveryElement(t *testing.T) {
 
 	// Assert
 	assert.ElementsMatch(t, input, expected)
+}
+
+func TestForEachToWorkFineConcurrently(t *testing.T) {
+	// Arrange
+	input1 := []int{1, 2, 3}
+	expected1 := make([]int, 0, len(input1))
+
+	input2 := []int{4, 5, 6}
+	expected2 := make([]int, 0, len(input2))
+
+	input3 := []int{7, 8, 9}
+	expected3 := make([]int, 0, len(input3))
+
+	sampleFun1 := func(x int) {
+		expected1 = append(expected1, x)
+	}
+
+	sampleFun2 := func(x int) {
+		expected2 = append(expected2, x)
+	}
+
+	sampleFun3 := func(x int) {
+		expected3 = append(expected3, x)
+	}
+
+	// Act
+	wg := &sync.WaitGroup{}
+	wg.Add(3)
+	go func() {
+		ForEach(input2, sampleFun2)
+		wg.Done()
+	}()
+
+	go func() {
+		ForEach(input1, sampleFun1)
+		wg.Done()
+	}()
+
+	go func() {
+		ForEach(input3, sampleFun3)
+		wg.Done()
+	}()
+
+	wg.Wait()
+
+	// Assert
+	assert.ElementsMatch(t, input1, expected1)
+	assert.ElementsMatch(t, input2, expected2)
+	assert.ElementsMatch(t, input3, expected3)
 }
 
 func TestMapToCallTheFunctionForEveryElementAndReturnTheExpectedResult(t *testing.T) {
